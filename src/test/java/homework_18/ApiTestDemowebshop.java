908e852740151becbda2a0ff7c166e177370db69
TatianaCheb <tanya.cheba12@gmail.com>
2022-04-27 18:57:44 +0300

    add homework 18

--- src/test/java/homework_18/ApiTestDemowebshop.java
+++ src/test/java/homework_18/ApiTestDemowebshop.java
@@ -1,6 +1,7 @@
 package homework_18;
 
 import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,7 +10,8 @@ import static org.hamcrest.Matchers.is;
 public class ApiTestDemowebshop {
 
     @Test
-    void addToCartWithCookie() {
+    @DisplayName("Добавление товара в wishlist")
+    void addToWishlist() {
         ValidatableResponse response =
                 given()
                         .log().all()
@@ -23,8 +25,35 @@ public class ApiTestDemowebshop {
                         .statusCode(200)
                         .body("success", is(true))
                         .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"));
-        System.out.println(response);
 
+    }
+
+    @Test
+    @DisplayName("Подписка на пустой email")
+    void subscribeEmptyTest() {
+        ValidatableResponse response =
+                given()
+                        .contentType("application/json; charset=utf-8")
+                        .cookie("Nop.customer=f3eb1b09-5d79-4e6a-8414-6b688f676df7;")
+                        .post("http://demowebshop.tricentis.com/subscribenewsletter")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("Success", is(false))
+                        .body("Result", is("Enter valid email"));
+    }
+
+    @Test
+    @DisplayName("Подписка на email")
+    void subscribeFullTest() {
+        ValidatableResponse response =
+                given()
+                        .formParam("email", "test@qa.guru")
+                        .post("http://demowebshop.tricentis.com/subscribenewsletter")
+                        .then()
+                        .statusCode(200)
+                        .body("Success", is(true))
+                        .body("Result", is("Thank you for signing up! A verification email has been sent. We appreciate your interest."));
     }
 }
 
