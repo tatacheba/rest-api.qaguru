5992b8e90805fb1aa61b56b260ea57ed1307cffe
TatianaCheb <tanya.cheba12@gmail.com>
2022-05-06 14:58:11 +0300

    added models

---
+++ src/test/java/models/GenerateTokenResponseLombok.java
@@ -0,0 +1,13 @@
+package models;
+
+import lombok.Data;
+
+@Data
+public class GenerateTokenResponseLombok {
+
+    private String token;
+    private String expires;
+    private String status;
+    private String result;
+
+}
